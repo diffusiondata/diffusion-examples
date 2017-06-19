39d1e5aa569d540abc7ec10fc4262c4d595785ec
Martin Cowie <mcowie@pushtechnology.com>
2017-06-19 14:34:17 +0100

    Update Diffusion Examples to 5.9.10

--- dotnet/GettingStarted/Publishing/Properties/AssemblyInfo.cs
+++ dotnet/GettingStarted/Publishing/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Publishing" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "The Diffusion .NET Client Library publishing example client." )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "Publishing" )]
-[assembly: AssemblyCopyright( "Copyright ©  2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
+[assembly: AssemblyCompany( "Push Technology Limited" )]
+[assembly: AssemblyProduct( "Diffusion" )]
+[assembly: AssemblyCopyright( "Copyright (c) 2016, 2017 Push Technology Limited" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
